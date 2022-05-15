ad7b7d8bdd3c67eba1c8f1834588dc817cc694dc
F3FFO <federico.pieranton@gmail.com>
2022-05-15 17:03:20 +0200

    Delete monospace font

--- common/install.sh
+++ common/install.sh
@@ -1,7 +1,6 @@
 MANUFACTER=$(getprop ro.product.manufacturer)
 
 ln -s $MODPATH/system/fonts/NotoSans-Regular.ttf $MODPATH/system/fonts/AndroidClock.ttf
-ln -s $MODPATH/system/fonts/NotoSansMono.ttf $MODPATH/system/fonts/DroidSansMono.ttf
 
 if [ $MANUFACTER = "Samsung" ]; then
 	ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/SamsungColorEmoji.ttf
