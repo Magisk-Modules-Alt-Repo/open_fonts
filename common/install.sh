e28078f3dde64e88ffb2afbd82a4a51c0addeb9a
F3FFO <federico.pieranton@gmail.com>
2022-05-22 16:45:50 +0200

    Optimized installation script

--- common/install.sh
+++ common/install.sh
@@ -1,40 +1,41 @@
 # environment
-EMOJI=false
-VERSION=$(getprop ro.system.build.version.release)
 SDK=$(getprop ro.system.build.version.sdk)
 MANUFACTER=$(getprop ro.product.manufacturer)
+EMOJI=false
 
 # replace
 ln -s $MODPATH/system/fonts/NotoSans-Regular.ttf $MODPATH/system/fonts/AndroidClock.ttf
 ln -s $MODPATH/system/fonts/NotoSans-Regular.ttf $MODPATH/system/fonts/DroidSans.ttf
 ln -s $MODPATH/system/fonts/NotoSans-Bold.ttf $MODPATH/system/fonts/DroidSans-Bold.ttf
 
-ui_print "- You are currently running Android $VERSION"
 if [ $SDK -ge 32 ]; then
-	ui_print '  Your system already supports the latest version of emoji'
-	# user input
-	ui_print '- Do you want to replace system emoji with Noto Emoji?'
+	ui_print ''
+	ui_print '- Your system already supports the latest version of emoji'
+	ui_print ''
+fi
+
+# user input
+ui_print '- Do you want to replace system emoji with Noto Emoji?'
+
+if [ $SDK -ge 32 ]; then
 	ui_print '  Vol+ = yes, Vol- = no[recommended]'
-	if chooseport 5; then
-		EMOJI=true
-	fi
 else
-	# user input
-	ui_print '- Do you want to replace system emoji with Noto Emoji?'
 	ui_print '  Vol+ = yes, Vol- = no'
-	if chooseport 5; then
-		EMOJI=true
-	fi
+fi
+
+if chooseport 5; then
+	EMOJI=true
 fi
 
 # replace emoji
 if $EMOJI; then
 	ui_print '  Downloading NotoColorEmoji...'
-	curl --output-dir "$MODPATH/system/fonts" -OL https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
+	curl -OLs --output-dir "$MODPATH/system/fonts" https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
 	ui_print '  NotoColorEmoji downloaded!'
-	
+	ui_print ''
 	ui_print '  Replacing system emojis...'
 	ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/NotoColorEmoji.ttf
+	sed -i 's@NotoColorEmojiFlags@NotoColorEmoji@g' $MODPATH/system/etc/fonts.xml
 	if [ $MANUFACTER = "Samsung" ]; then
 		ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/SamsungColorEmoji.ttf
 	elif [ $MANUFACTER = "LGE" ]; then
