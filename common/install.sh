a0019154167868b43914f062a15caebe17bf6ecc
F3FFO <federico.pieranton@gmail.com>
2022-05-22 14:27:28 +0200

    Decreased module size

--- common/install.sh
+++ common/install.sh
@@ -9,16 +9,18 @@ ui_print "  Vol+ = yes, Vol- = no"
 if chooseport 5; then
 	# replace emoji
 	MANUFACTER=$(getprop ro.product.manufacturer)
+	ui_print '  Downloading NotoColorEmoji...'
+	curl --output-dir "$MODPATH/system/fonts" -OL https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
+	ui_print '  NotoColorEmoji downloaded!'
+	
 	ui_print '  Replacing system emojis...'
-
-	ln -s $MODPATH/system/fonts/NotoEmoji.ttf $MODPATH/system/fonts/NotoColorEmoji.ttf
+	ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/NotoColorEmoji.ttf
 	if [ $MANUFACTER = "Samsung" ]; then
-		ln -s $MODPATH/system/fonts/NotoEmoji.ttf $MODPATH/system/fonts/SamsungColorEmoji.ttf
+		ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/SamsungColorEmoji.ttf
 	elif [ $MANUFACTER = "LGE" ]; then
-		ln -s $MODPATH/system/fonts/NotoEmoji.ttf $MODPATH/system/fonts/LGNotoColorEmoji.ttf
+		ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/LGNotoColorEmoji.ttf
 	elif [ $MANUFACTER = "HTC" ]; then
-		ln -s $MODPATH/system/fonts/NotoEmoji.ttf $MODPATH/system/fonts/HTC_ColorEmoji.ttf
+		ln -s $MODPATH/system/fonts/NotoColorEmoji.ttf $MODPATH/system/fonts/HTC_ColorEmoji.ttf
 	fi
-
 	ui_print '  System emojis replaced!'
 fi
