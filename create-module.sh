30202341466e1c2375044b53af31ef621bfddd15
F3FFO <federico.pieranton@gmail.com>
2022-10-30 20:53:14 +0100

    Update script to create module

--- create-module.sh
+++ create-module.sh
@@ -2,4 +2,4 @@
 MODULE_NAME=open-fonts.zip
 
 rm -r $MODULE_NAME
-zip -r $MODULE_NAME ./* -x ./git/\* ./.github/\* ./img/\* ./common/tools/tools/\* ./README.md\* ./CHANGELOG.md\* ./gitignore\*
+zip -r $MODULE_NAME ./* -x ./git/\* ./.github/\* ./img/\* ./common/tools/tools/\* ./sources/\* ./README.md\* ./CHANGELOG.md\* ./gitignore\*
