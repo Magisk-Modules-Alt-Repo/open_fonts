458e789c0ee483ea7fbe3a1fa7516e057353cdb4
F3FFO <federico.pieranton@gmail.com>
2023-04-23 16:10:47 +0200

    Update create-module script

--- create-module.sh
+++ create-module.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MODULE_NAME=open-fonts.zip
 
-rm -r $MODULE_NAME
-zip -r $MODULE_NAME ./* -x ./git/\* ./.github/\* ./img/\* ./common/tools/tools/\* ./sources/\* ./README.md\* ./CHANGELOG.md\* ./gitignore\*
+echo "deleting old files..."
+rm -rf $MODULE_NAME
+echo -e "ok!\n\ncreating module zip..."
+7z a -tzip -r $MODULE_NAME * -xr!.git* -xr!img -xr!common/tools/tools -xr!sources -x!*.md -x!create-module.sh >& /dev/null
